0e850d656bf0a48ba11f9669d1768097c5740f02
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-12-01 13:07:31 +0100

    Initial push to solve the quest Design Pattern 01: Singleton

--- Transactions.java
+++ Transactions.java
@@ -1,3 +1,5 @@
+import singleton.Bank;
+
 public class Transactions {
 
     public static void transactions() {
@@ -13,7 +15,10 @@ public class Transactions {
     public static void balance() {
 
         // TODO 2 : call singleton and get total
-        int total = 0;
+
+        Singleton singleton = Singleton.getInstance();
+        int total = singleton.getBank().getTotal();
         System.out.printf("Your balance is:%d%n", total);
+
     }
 }
